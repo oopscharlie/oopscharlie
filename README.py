1235327b14199c7d574c59da4aca651a386ebccc
JIHOON <a93866244@gmail.com>
2025-05-13 11:11:44 +0800

    Update README.py

--- README.py
+++ README.py
@@ -7,6 +7,6 @@
 - ⚡ Fun fact: ...
 
 <!---
-oopscharlie/oopscharlie is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+oopscharlie/oopscharlie is a ✨ special ✨ repository because its `README.py` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
 --->
